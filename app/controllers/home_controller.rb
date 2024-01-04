c41dfaa86960bf3e3f4fd8a987e26de7f7806ffc
Marius Moldovan <m7marius7@gmail.com>
2024-01-04 14:37:10 +0200

    primul commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+
+class HomeController < ApplicationController
+
+    def index
+        @main_categories = Category.take(4)
+    end
+    
+end
---
+++ config/routes.rb
@@ -0,0 +1,30 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    resources :orders
+    resources :products do
+      resources :stocks
+    end
+    resources :categories
+  end
+  devise_for :admins
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  resources :categories, ony: [:show]
+  resources :products, ony: [:show]
+
+
+  get "admin" => "admin#index"
+  get "cart" => "carts#show"
+  post "checkout" => "checkouts#create"
+  get "success" => "checkouts#success"
+  get "cancel" => "checkouts#cancel"
+  post "webhooks" => "webhooks#stripe"
+end
+
